6bfd46d7d841ec79efe414be0ef6ba65b960597c
Gopal Jee <gopaljee9708@gmail.com>
2026-07-11 15:07:02 +0530

    Complete BE-01: Build first API endpoint

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello Backend AI Engineering!"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
